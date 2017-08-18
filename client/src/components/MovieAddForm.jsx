b7513b3280c4ab540d0abffdc7e91990ee06fb01
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-08-18 12:16:13 -0400

    add new Movie components

---
+++ client/src/components/MovieAddForm.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class MovieAddForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      description: '',
+      genre: '',
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    return (
+      <div className='add'>
+        <form onSubmit={(event) => this.props.handleMovieSubmit(event, this.state.title, this.state.description, this.state.genre)}>
+          <label> TITLE
+            <input type='text' name='title' placeholder='Title' value={this.state.title} onChange={this.handleInputChange} />
+          </label>
+          <label> DESCRIPTION
+            <input type='text' name='description' placeholder='Description' value={this.state.description} onChange={this.handleInputChange} />
+          </label>
+          <label> GENRE
+            <input type='text' name='genre' placeholder='Genre' value={this.state.genre} onChange={this.handleInputChange} />
+          </label>
+          <input type='submit' value='Add Movie' />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default MovieAddForm;
---
+++ client/src/components/MoviesList.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import Movie from './Movie';
+import MovieAddForm from './MovieAddForm';
+import MovieEditForm from './MovieEditForm';
+
+class MoviesList extends Component {
+  render() {
+    return (
+      <div className='movies-list'>
+        <MovieAddForm handleMovieSubmit={this.props.handleMovieSubmit} />
+        {this.props.movieData.map((movie) => {
+          if (this.props.currentMovieId === movie.id) {
+            return <MovieEditForm key={movie.id} movie={movie} handleMovieEditSubmit={this.props.handleMovieEditSubmit} />
+          } else return <Movie key={movie.id} movie={movie} selectEditedMovie={this.props.selectEditedMovie} />
+        })}
+      </div>
+    )
+  }
+}
+
+export default MoviesList;
