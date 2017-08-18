51a641d94ad88e1fa898905f08de191d24093488
dan martin <dmartss13@gmail.com>
2017-08-18 10:37:56 -0400

    app.js first steps

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const logger = require('moregan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+const app = express();
+require('dotenv').config();
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(
+  session({
+    key: process.env.SECRET_KEY,
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+  });
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`~~~Dream~~~Team~~~ up in here running on dat port ${PORT}`);
+});
