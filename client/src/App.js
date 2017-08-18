5116752da5a17c399dc0876b6efdb77de750e1d2
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-08-18 12:14:39 -0400

    install axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
-
 import axios from 'axios';
+import logo from './logo.svg';
+
+import './App.css';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
