79f9ac57b2411fcb8df35cab85e0e3d979c71dab
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-08-19 12:51:21 -0400

    but y so many spaces tho

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return(
         <div className="home">
             <h1> Welcome to our movies app. </h1>
-            <h1>  This is the ~ Dream ~ Team ~  </h1>
+            <h1> This is the ~ Dream ~ Team ~ </h1>
         </div>
     )
 }
