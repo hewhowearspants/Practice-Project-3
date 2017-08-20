c44fb8650aba4286684c47d00549576dccea5441
Mark Ma <mazheng1022@hotmail.com>
2017-08-20 11:28:52 -0400

    add comment things to movie-routes

--- routes/movie-routes.js
+++ routes/movie-routes.js
@@ -10,5 +10,7 @@ movieRoutes.post('/',authHelpers.loginRequired,moviesController.create);
 movieRoutes.get('/:id',moviesController.show);
 movieRoutes.put('/:id',authHelpers.loginRequired,moviesController.update);
 movieRoutes.delete('/:id',authHelpers.loginRequired,moviesController.delete);
+movieRoutes.post('/comments/:id',moviesController.createComment);
+movieRoutes.get('/comments/:id',moviesController.showMovieComments);
 
 module.exports=movieRoutes;
