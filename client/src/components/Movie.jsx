cee6e90846e18ceaf2171c8ed0d19dbf7e699235
dan martin <dmartss13@gmail.com>
2017-08-20 20:38:34 -0400

    toggle class for favMovie

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import React, { Component } from 'react';
 
@@ -64,13 +65,16 @@ class Movie extends Component{
   render() {
     return (
       <div className='movie'>
+      <div className={`my-movie ${props.movieId === props.movie.id ? 'movieId' : ''}`}>
+        <i onClick={() => props.favMovie(props.movie.id)} className="fa fa-star fa-2x" />
+      </div>
         <h3>{this.props.movie.title}</h3>
         <p>{this.props.movie.description}</p>
         <p>Genre: {this.props.movie.genre}</p>
         {this.props.auth ? <span className='edit' onClick={() => this.props.selectEditedMovie(this.props.movie.id)}>Edit</span> : '' }
         <span className='edit' onClick={this.expandForm}>See Comments</span>
         <div className={`comment-box ${this.state.commentExpand}`}>
-          {this.props.auth ? 
+          {this.props.auth ?
             <CommentAddForm commentData={this.state.commentData} movie={this.props.movie} updatePage={this.updatePage}/>
           : '' }
           <div className='comment-list'>
@@ -83,4 +87,3 @@ class Movie extends Component{
 }
 
 export default Movie;
-
