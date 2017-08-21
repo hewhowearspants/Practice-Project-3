c7dbc6aa9bfded5561ada086b3ce46329af61dc7
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-08-20 23:04:39 -0400

    moved get user favorite route above :ids so it wont be a parameter

--- routes/movie-routes.js
+++ routes/movie-routes.js
@@ -7,11 +7,14 @@ const moviesController=require('../controllers/movies-controller');
 movieRoutes.get('/',moviesController.index);
 movieRoutes.post('/',authHelpers.loginRequired,moviesController.create);
 
+movieRoutes.get('/fav', authHelpers.loginRequired, moviesController.getFavorites);
+
 movieRoutes.get('/:id',moviesController.show);
 movieRoutes.put('/:id',authHelpers.loginRequired,moviesController.update);
 movieRoutes.delete('/:id',authHelpers.loginRequired,moviesController.delete);
 movieRoutes.post('/comments/:id',authHelpers.loginRequired,moviesController.createComment);
 movieRoutes.get('/comments/:id',moviesController.showMovieComments);
+
 
 movieRoutes.post('/:id/fav', authHelpers.loginRequired, moviesController.addFavorite);
 movieRoutes.delete('/:id/fav', authHelpers.loginRequired, moviesController.deleteFavorite);
